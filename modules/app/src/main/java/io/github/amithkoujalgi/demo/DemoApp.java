b944687bdfbbba3fbfe80225651a76864124d7ee
Amith Koujalgi <amith.koujalgi@razorthink.com>
2023-11-20 18:11:58 +0530

    init

--- modules/app/src/main/java/io/github/amithkoujalgi/demo/DemoApp.java
+++ modules/app/src/main/java/io/github/amithkoujalgi/demo/DemoApp.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"io.github.amithkoujalgi.demo.config", "io.github.amithkoujalgi.demo.controllers", "io.github.amithkoujalgi.demo.repositories", "io.github.amithkoujalgi.demo.initializers"})
-@OpenAPIDefinition(info = @Info(title = "Demo Server"))
+@OpenAPIDefinition(info = @Info(title = "Demo App"))
 public class DemoApp {
 
     public static void main(String[] args) {
