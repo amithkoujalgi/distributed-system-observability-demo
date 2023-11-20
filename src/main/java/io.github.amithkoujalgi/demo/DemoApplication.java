30b53942bd60f5716e8808d42c5b3d97883f2f04
Amith Koujalgi <amith.koujalgi@razorthink.com>
2023-11-20 13:05:27 +0530

    init

--- src/main/java/io.github.amithkoujalgi/demo/DemoApplication.java
+++ src/main/java/io.github.amithkoujalgi/demo/DemoApplication.java
@@ -4,10 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"io.github.amithkoujalgi.demo.config", "io.github.amithkoujalgi.demo.controllers", "io.github.amithkoujalgi.demo.repositories", "io.github.amithkoujalgi.demo.initializers"})
+@SpringBootApplication(scanBasePackages = {"io.github.amithkoujalgi.demo.config", "io.github.amithkoujalgi.demo.controllers", "io.github.amithkoujalgi.demo.repositories", "io.github.amithkoujalgi.demo.initializers"})
 @OpenAPIDefinition(info = @Info(title = "Demo Server"))
 public class DemoApplication {
 
