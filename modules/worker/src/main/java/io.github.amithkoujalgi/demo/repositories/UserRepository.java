9ba87c49c7bd820b4287dfe8f6d030e872a3738c
Amith Koujalgi <amith.koujalgi@razorthink.com>
2023-11-20 14:57:12 +0530

    init

--- modules/worker/src/main/java/io.github.amithkoujalgi/demo/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.amithkoujalgi.demo.repositories;
-
-import io.github.amithkoujalgi.demo.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
--- modules/worker/src/main/java/io.github.amithkoujalgi/demo/tests/Tests.java
+++
@@ -1,6 +0,0 @@
-package io.github.amithkoujalgi.demo.tests;
-
-public class Tests {
-    public static void main(String[] args) {
-    }
-}
