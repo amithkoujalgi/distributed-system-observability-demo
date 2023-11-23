7e7b9e9e18e76dd4aaece990dc4b11d600519d5f
Amith Koujalgi <amith.koujalgi@razorthink.com>
2023-11-23 18:12:21 +0530

    init

--- modules/producer/src/main/java/io/github/amithkoujalgi/demo/ProducerBatchProcessingApplication.java
+++ modules/producer/src/main/java/io/github/amithkoujalgi/demo/ProducerBatchProcessingApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "io.github.amithkoujalgi.demo")
-public class BatchProcessingApplication {
-
+public class ProducerBatchProcessingApplication {
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(BatchProcessingApplication.class);
+        SpringApplication application = new SpringApplication(ProducerBatchProcessingApplication.class);
         System.exit(SpringApplication.exit(application.run(args)));
     }
 }
