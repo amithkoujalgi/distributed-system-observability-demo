7b5a0fcb699898881e47b1672a4330f0e1c850d8
Amith Koujalgi <amith.koujalgi@razorthink.com>
2023-11-24 10:19:48 +0530

    init

--- modules/consumer/src/main/java/io/github/amithkoujalgi/demo/consumer/writers/RedisInstrumentWriter.java
+++ modules/consumer/src/main/java/io/github/amithkoujalgi/demo/consumer/writers/RedisInstrumentWriter.java
@@ -5,22 +5,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RedisInstrumentWriter implements ItemWriter<Instrument> {
     private static final Logger log = LoggerFactory.getLogger(RedisInstrumentWriter.class);
 
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Value("${infrastructure.topics.price-changes}")
     private String topicPriceUpdates;
+
+    @Value("${infrastructure.redis.keys.stocks}")
+    private String redisHashKeyPrefixStocks;
+
 
     @Override
     public void write(Chunk<? extends Instrument> chunk) {
         log.info("Writing instruments to Redis...");
         for (Instrument instrument : chunk.getItems()) {
-
+            redisTemplate.opsForHash().put(redisHashKeyPrefixStocks, instrument.getName(), instrument.toJSON());
         }
     }
 }
