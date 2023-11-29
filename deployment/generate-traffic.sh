b1e6845a8557a19842b6d89058ceef88881acf5f
Amith Koujalgi <amith.koujalgi@razorthink.com>
2023-11-29 15:59:38 +0530

    init

--- deployment/generate-traffic.sh
+++ deployment/generate-traffic.sh
@@ -9,3 +9,7 @@ http POST http://localhost:9903/api/trade/order \
   quantity:=20 \
   userId=1 \
   type=BUY
+
+http GET http://localhost:9902/api/instrument/stocks \
+  accept:application/json \
+  access-token:c96cf9ae-a0ce-40ba-91b8-8a636a38a87d
