1496ee2214f727a5d7709a1227b1cf87d2cededa
Amith Koujalgi <amith.koujalgi@razorthink.com>
2023-12-01 12:34:35 +0530

    init

--- deployment/compose/deploy.sh
+++ deployment/compose/deploy.sh
@@ -1,2 +1,4 @@
-docker-compose -f ./docker-compose.yaml down
+#!/bin/bash
+docker-compose -f ./docker-compose.yaml rm -fsv
+docker-compose -f ./docker-compose.yaml down -v
 docker-compose -f ./docker-compose.yaml up --remove-orphans
